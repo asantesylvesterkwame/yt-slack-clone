c8cf54dad269552c8062f52a7327d0510fded237
asantesylvesterkwame <intelligentson17@gmail.com>
2026-09-25 12:33:45 +0000

    feat: Added UserButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import UserButton from "@/features/auth/components/UserButton";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div>
-      Home
-      <Button onClick={() => void signOut()}>Log Out</Button>
+      <UserButton />
     </div>
   );
 }
